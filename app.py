7d524b13f121cf1611b74d23f462501f9ecf64cc
justanotheratom <sanketpatel.1805@gmail.com>
2022-11-22 15:08:07 -0800

    persist user data to file

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ import toml
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 import replicate
+from datetime import datetime
+from dataclasses import dataclass
+from collections import defaultdict
+import tempfile
+from dataclass_csv import DataclassReader, DataclassWriter
 
 #-------------------------------------------------------------------------------
 
@@ -26,19 +31,120 @@ WEBHOOK_URL_PATH = "/%s/" % (app.config['TELEGRAM_API_TOKEN'])
 os.environ['REPLICATE_API_TOKEN'] = app.config['REPLICATE_API_TOKEN']
 
 #-------------------------------------------------------------------------------
+
+@dataclass
+class JournalEntry:
+    timestamp: datetime
+    text: str
+    voice_file_id: str
+
+@dataclass
+class QAEntry:
+    timestamp: datetime
+    question: str
+    answer: str
+    voice_file_id: str
+
+@dataclass
+class UserData:
+    journal: list
+    qa: list
+
+userdata = {}
+
+#-------------------------------------------------------------------------------
+# PERSISTANCE LAYER
+#-------------------------------------------------------------------------------
+
+def user_data_directory(user_id):
+    return os.path.join(app.config['DATA_DIRECTORY'], str(user_id))
+
+def user_journal_file(user_id):
+    return os.path.join(user_data_directory(user_id), 'journal.tsv')
+
+def user_qa_file(user_id):
+    return os.path.join(user_data_directory(user_id), 'qa.tsv')
+
+def hydrate_user_data(user_id):
+    if not user_id in userdata:
+        userdata[user_id] = UserData([], [])
+        journal_file = user_journal_file(user_id)
+        if os.path.exists(journal_file):
+            with open(journal_file, "r") as f:
+                reader = DataclassReader(f, JournalEntry, delimiter='\t')
+                for row in reader:
+                    userdata[user_id].journal.append(row)
+        qa_file = user_qa_file(user_id)
+        if os.path.exists(qa_file):
+            with open(qa_file, "r") as f:
+                reader = DataclassReader(f, QAEntry, delimiter='\t')
+                for row in reader:
+                    userdata[user_id].qa.append(row)
+
+#-------------------------------------------------------------------------------
 # HELPERS
 #-------------------------------------------------------------------------------
 
 def transcribe(file_id):
+
     file_info = bot.get_file(file_id)
     file_content = bot.download_file(file_info.file_path)
-    f = open('foo.ogg', 'w+b')
-    f.write(file_content)
-    f.close()
-    model = replicate.models.get("openai/whisper")
-    version = model.versions.get("089ea17a12d0b9fc2f81d620cc6e686de7a156007830789bf186392728ac25e8")
-    result = version.predict(audio=Path('foo.ogg'))
-    return result['transcription']
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        tmp_path: Path = Path(tmp_dir) / Path('foo.ogg')
+        with open(tmp_path, "w+b") as f:
+            f.write(file_content)
+        model = replicate.models.get("openai/whisper")
+        version = model.versions.get("089ea17a12d0b9fc2f81d620cc6e686de7a156007830789bf186392728ac25e8")
+        result = version.predict(audio=tmp_path, model='large')
+        return result['transcription']
+
+def send_response(
+    message,
+    response,
+    markup=telebot.types.ReplyKeyboardRemove(selective=False)
+    ):
+    bot.send_message(
+        message.chat.id,
+        response,
+        reply_markup=markup
+    )
+
+def is_question(text):
+    return text.endswith('?')
+
+def process_question(user_id, qa_entry):
+    hydrate_user_data(user_id)
+    userdata[user_id].qa.append(qa_entry)
+    os.makedirs(user_data_directory(user_id), exist_ok=True)
+    qa_file = user_qa_file(user_id)
+    if not os.path.exists(qa_file):
+        with open(qa_file, "w") as f:
+            w = DataclassWriter(f, [], QAEntry, delimiter='\t')
+            w.write()
+    with open(qa_file, 'a+') as f:
+        w = DataclassWriter(f, [qa_entry], QAEntry, delimiter='\t')
+        w.write(skip_header=True)
+
+def process_journal_entry(user_id, journal_entry):
+    hydrate_user_data(user_id)
+    userdata[user_id].journal.append(journal_entry)
+    os.makedirs(user_data_directory(user_id), exist_ok=True)
+    journal_file = user_journal_file(user_id)
+    if not os.path.exists(journal_file):
+        with open(journal_file, "w") as f:
+            w = DataclassWriter(f, [], JournalEntry, delimiter='\t')
+            w.write()
+    with open(journal_file, 'a+') as f:
+        w = DataclassWriter(f, [journal_entry], JournalEntry, delimiter='\t')
+        w.write(skip_header=True)
+
+def process_text(user_id, timestamp, text, voice_file_id): 
+    timestamp = datetime.fromtimestamp(timestamp),
+    if (is_question(text)):
+        process_question(user_id, QAEntry(timestamp, text, None, voice_file_id))
+    else:
+        process_journal_entry(user_id, JournalEntry(timestamp, text, voice_file_id))
 
 #-------------------------------------------------------------------------------
 # ROUTES
@@ -64,8 +170,8 @@ def webhook():
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(
-        message.chat.id,
+    send_response(
+        message,
         (
             "Hello human. Tell me things you know you might forget. "
             "Then later, ask me questions to refresh your memory.\n\n"
@@ -86,7 +192,6 @@ def gen_markup():
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-    print(call)
     if call.data == "cb_yes":
         bot.answer_callback_query(call.id, "Ok, if you say so. Done.")
     elif call.data == "cb_no":
@@ -96,16 +201,16 @@ def callback_query(call):
 def send_welcome(message):
     markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     markup.add(KeyboardButton('Yes, I confirm.'))
-    bot.send_message(
-        message.chat.id,
+    send_response(
+        message,
         "What?? Are you sure you want to delete all your data?",
-        reply_markup=markup
+        markup
     )
 
 @bot.message_handler(commands=['givefeedback'])
 def send_welcome(message):
-    bot.send_message(
-        message.chat.id,
+    send_response(
+        message,
         (
             "Me so happy. Thank you for feedback."
         )
@@ -116,15 +221,25 @@ def send_welcome(message):
 #-------------------------------------------------------------------------------
 
 @bot.message_handler(content_types=['text'])
-def echo_message(message):
-    print('text')
-    bot.reply_to(message, message.text)
+def text_sink(message):
+    process_text(
+        message.from_user.id,
+        message.date,
+        message.text,
+        None
+    )
 
 @bot.message_handler(content_types=['voice'])
 def audio_sink(message):
-    bot.send_message(
-        message.chat.id,
-        transcribe(message.voice.file_id))
+    voice_file_id = message.voice.file_id
+    text = transcribe(voice_file_id)
+    process_text(
+        message.from_user.id,
+        message.date,
+        text,
+        voice_file_id
+    )
+    send_response( message, text)
 
 #-------------------------------------------------------------------------------
 
